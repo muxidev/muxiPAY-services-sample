07a13fc16cdce1021da54206bba8031011db1f76
Leonardo Saippa <lsaippa@muxi.com.br>
2019-06-19 09:42:43 -0300

    Creates Payment screen

--- app/src/main/java/muxi/sample/ui/present_card/MainActivity.kt
+++ app/src/main/java/muxi/sample/ui/present_card/MainActivity.kt
@@ -1,19 +1,16 @@
 package muxi.sample.ui.present_card
 
-import android.content.ComponentName
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import muxi.sample.Constants
 import muxi.sample.R
-import muxi.sample.data.MPSTransaction
-import muxi.sample.data.MPSTransactionResult
 import muxi.sample.service.MPSManager
+import muxi.sample.ui.CallbackManager
 import muxi.sample.ui.dialog.DialogHelper
 import muxi.sample.ui.present_card.tasks.DeconfigureTask
 import muxi.sample.ui.present_card.tasks.InitTask
-import muxi.sample.ui.present_card.tasks.TransactionTask
 import org.jetbrains.anko.toast
 
 class MainActivity : AppCompatActivity() {
@@ -39,10 +36,6 @@ class MainActivity : AppCompatActivity() {
     private val ignorePendingTransaction = true
     private val cnpj = "1234"
 
-    val value = "2200"
-    val type = MPSTransaction.TransactionType.CREDIT
-    val installments = 1
-    val mpsTransaction = mountTransaction(value,type,installments)
     val dialogHelper = DialogHelper.newInstance()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -54,12 +47,11 @@ class MainActivity : AppCompatActivity() {
             InitTask(mpsManager!!,showMessage,cnpj).execute()
         }
         btnTransact.setOnClickListener {
-            dialogHelper.showLoadingDialog(this, View.VISIBLE)
-            TransactionTask(mpsManager!!,mpsTransaction!!,Constants.TransactionState.payment).execute()
+            startActivity(Intent(this, PaymentActivity::class.java))
         }
         btnCancel.setOnClickListener {
-            dialogHelper.showLoadingDialog(this, View.VISIBLE)
-            TransactionTask(mpsManager!!,mpsTransaction!!,Constants.TransactionState.cancel).execute()
+          //  dialogHelper.showLoadingDialog(this, View.VISIBLE)
+           // TransactionTask(mpsManager!!,mpsTransaction!!,Constants.TransactionState.cancel).execute()
         }
         btnDeconfigure.setOnClickListener {
             dialogHelper.showLoadingDialog(this, View.GONE)
@@ -72,124 +64,18 @@ class MainActivity : AppCompatActivity() {
 
     }
 
-    private fun mountTransaction(value: String, type: MPSTransaction.TransactionType,
-                                 installments: Int): MPSTransaction? {
-        val transaction = MPSTransaction()
-        transaction.amount = value
-        transaction.currency = MPSTransaction.CurrencyType.BRL
-        transaction.type = type
-
-        return transaction
-
-    }
-
     override fun onStart() {
         super.onStart()
-
         if(mpsManager == null)
             mpsManager = MPSManager.getInstance(this.applicationContext)
 
-
-        val bindService = mpsManager!!.bindService(applicationContext)
-        mpsManager!!.setMpsManagerCallback(mpsManagerCallback)
-
+        mpsManager!!.bindService(applicationContext)
+        mpsManager!!.setMpsManagerCallback(CallbackManager.newInstance(this, dialogHelper).mpsManagerCallback)
     }
 
     override fun onDestroy() {
         mpsManager!!.unbindService(this)
         super.onDestroy()
-    }
-
-
-
-    private val mpsManagerCallback = object : MPSManager.MPSManagerCallback {
-        override fun onCancelAnswer(mpsTransactionResult: MPSTransactionResult?) {
-            this@MainActivity.runOnUiThread{
-                dialogHelper.hideLoadingDialog()
-                var body = ""
-                var toastAnswer = ""
-                //TODO add treatment to !! , when cannot have null branch
-                when(mpsTransactionResult!!.transactionStatus) {
-                    MPSTransactionResult.TransactionStatus.SUCCESS -> {
-                        body = resources.getString(R.string.cancelSuccess)
-                        toastAnswer = mpsTransactionResult.clientReceipt
-                    }
-                    MPSTransactionResult.TransactionStatus.ERROR -> {
-                        body = resources.getString(R.string.cancelError)
-                        toastAnswer = mpsTransactionResult.descriptionError
-                    }
-                }
-                dialogHelper.showTransactionDialog(this@MainActivity,
-                    mpsTransactionResult.transactionStatus.name,
-                    body
-                )
-                toast(toastAnswer)
-            }
-
-        }
-
-        override fun onDeconfigureAnswer(mpsTransactionResult: MPSTransactionResult?) {
-            this@MainActivity.runOnUiThread {
-                dialogHelper.hideLoadingDialog()
-                var body = ""
-                when(mpsTransactionResult!!.transactionStatus.name){
-                    MPSTransactionResult.TransactionStatus.SUCCESS.name -> body = resources.getString(R.string.deconfigureSuccess)
-                    MPSTransactionResult.TransactionStatus.ERROR.name->body = resources.getString(R.string.deconfigureError)
-                }
-                dialogHelper.showInitDialog(this@MainActivity,
-                    mpsTransactionResult.transactionStatus.name,
-                    body
-                )
-                toast(mpsTransactionResult.transactionStatus.name)
-
-            }
-        }
-
-        override fun onInitAnswer(mpsTransactionResult: MPSTransactionResult?) {
-            this@MainActivity.runOnUiThread {
-
-                dialogHelper.hideLoadingDialog()
-                var body = ""
-                when(mpsTransactionResult!!.transactionStatus.name){
-                    MPSTransactionResult.TransactionStatus.SUCCESS.name -> body = resources.getString(R.string.initSuccess)
-                    MPSTransactionResult.TransactionStatus.ERROR.name->body = resources.getString(R.string.initError)
-                }
-                dialogHelper.showInitDialog(this@MainActivity,
-                    mpsTransactionResult.transactionStatus.name,
-                    body
-                )
-                toast(mpsTransactionResult.transactionStatus.name)
-
-            }
-        }
-
-        override fun onTransactionAnswer(mpsTransactionResult: MPSTransactionResult?) {
-            this@MainActivity.runOnUiThread{
-                dialogHelper.hideLoadingDialog()
-                var body = ""
-                var toastAnswer = ""
-                when(mpsTransactionResult!!.transactionStatus) {
-                    MPSTransactionResult.TransactionStatus.SUCCESS -> {
-                        body = resources.getString(R.string.transactionSuccess)
-                        toastAnswer = mpsTransactionResult.clientReceipt
-                    }
-                    MPSTransactionResult.TransactionStatus.ERROR -> {
-                        body = resources.getString(R.string.transactionError)
-                        toastAnswer = mpsTransactionResult.descriptionError
-                    }
-                }
-                dialogHelper.showTransactionDialog(this@MainActivity,
-                    mpsTransactionResult.transactionStatus.name,
-                    body
-                )
-                toast(toastAnswer)
-            }
-        }
-
-        override fun onServiceDisconnected(componentName: ComponentName?) {
-            toast(componentName.toString())
-        }
-
     }
 
 }
---
+++ app/src/main/java/muxi/sample/ui/present_card/PaymentActivity.kt
@@ -0,0 +1,244 @@
+package muxi.sample.ui.present_card
+
+import android.content.ComponentName
+import android.graphics.PorterDuff
+import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.util.Log
+import android.view.MotionEvent
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Button
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_payment.*
+import muxi.sample.Constants
+import muxi.sample.R
+import muxi.sample.data.MPSTransaction
+import muxi.sample.data.MPSTransactionResult
+import muxi.sample.service.MPSManager
+import muxi.sample.ui.CallbackManager
+import muxi.sample.ui.dialog.DialogHelper
+import muxi.sample.ui.present_card.tasks.TransactionTask
+import org.jetbrains.anko.toast
+import java.lang.Double.parseDouble
+import java.text.NumberFormat
+
+class PaymentActivity : AppCompatActivity(), AdapterView.OnItemSelectedListener {
+
+    private val TAG = PaymentActivity::class.java.simpleName
+
+    private var mpsManager: MPSManager? = null
+    val dialogHelper = DialogHelper.newInstance()
+
+
+    val type = MPSTransaction.TransactionType.CREDIT
+    var installments = 1
+    var list_of_items = arrayOf("installments","1","2","3","4","5","6",
+        "7","8","9","10","11","12")
+    private var paymentType = ""
+    private var currentValue = ""
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_payment)
+
+        spinner!!.onItemSelectedListener = this
+
+        val mpsTransaction =  mountTransaction("1000",type,installments)
+
+        val aa = ArrayAdapter(this, R.layout.simple_spinner_item, list_of_items)
+
+        aa.setDropDownViewResource(R.layout.simple_spinner_dropdown_item)
+
+        spinner!!.adapter = aa
+
+        btn_pay!!.setOnClickListener {
+            DialogHelper.newInstance().showLoadingDialog(this,View.VISIBLE)
+            dialogHelper.showLoadingDialog(this, View.VISIBLE)
+            TransactionTask(mpsManager!!,mpsTransaction!!, Constants.TransactionState.payment).execute()
+        }
+
+        btn_credit!!.setOnClickListener{
+            buttonEffect(btn_credit, Constants.TypePayment.CREDIT,btn_debit,btn_voucher)
+        }
+
+        btn_debit!!.setOnClickListener{
+            buttonEffect(btn_debit, Constants.TypePayment.DEBIT,btn_credit,btn_voucher)
+        }
+
+        btn_voucher!!.setOnClickListener {
+            buttonEffect(btn_voucher, Constants.TypePayment.VOUCHER,btn_credit,btn_debit)
+        }
+
+        et_value.addTextChangedListener(object: TextWatcher {
+            override fun afterTextChanged(s: Editable?) {
+
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if(s.toString() != currentValue){
+                    et_value.removeTextChangedListener(this)
+
+                    var cleanString: String = s.toString().replace("[$,.]", "")
+                    Log.d(TAG, "cleasnString:$cleanString")
+
+                    var parsed: Double = parseDouble(cleanString)
+                    Log.d(TAG, "parsed:$parsed")
+
+                    var formatted: String = NumberFormat.getCurrencyInstance().format((parsed/100))
+                    Log.d(TAG, "formatted:$formatted")
+
+                    currentValue = formatted
+                    et_value.setText(formatted)
+                    et_value.setSelection(formatted.length)
+                    et_value.addTextChangedListener(this)
+                }
+            }
+
+        })
+
+    }
+    override fun onNothingSelected(parent: AdapterView<*>?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+    override fun onItemSelected(parent: AdapterView<*>?, view: View?, position: Int, id: Long) {
+        Log.d(TAG, "onItemSelected $id position $position")
+    }
+
+    override fun onStart() {
+        super.onStart()
+
+        if(mpsManager == null)
+            mpsManager = MPSManager.getInstance(this.applicationContext)
+
+        val bindService = mpsManager!!.bindService(applicationContext)
+
+        val callbackManager = CallbackManager.newInstance(this, dialogHelper)
+        mpsManager!!.setMpsManagerCallback(callbackManager.mpsManagerCallback)
+
+    }
+
+    private fun mountTransaction(value: String, type: MPSTransaction.TransactionType,
+                                 installments: Int): MPSTransaction? {
+        val transaction = MPSTransaction()
+        transaction.amount = value
+        transaction.currency = MPSTransaction.CurrencyType.BRL
+        transaction.type = type
+
+        return transaction
+
+    }
+    private fun buttonEffect(buttonPressed: Button, type: Constants.TypePayment, buttonUnpressed: Button, buttonUnpressedTwo: Button) {
+
+        buttonPressed.backgroundTintList = ContextCompat.getColorStateList(this,R.color.color_base)
+        buttonPressed.setTextColor(ContextCompat.getColor(this,R.color.color_text_pressed))
+
+        buttonUnpressed.backgroundTintList = ContextCompat.getColorStateList(this,R.color.color_btn_unpressed)
+        buttonUnpressed.setTextColor(ContextCompat.getColor(this,R.color.color_base))
+
+        buttonUnpressedTwo.backgroundTintList = ContextCompat.getColorStateList(this,R.color.color_btn_unpressed)
+        buttonUnpressedTwo.setTextColor(ContextCompat.getColor(this,R.color.color_base))
+
+        paymentType = type.name
+        Log.d(TAG, "Type Payment: $paymentType")
+    }
+
+    private val mpsManagerCallback = object : MPSManager.MPSManagerCallback {
+        override fun onCancelAnswer(mpsTransactionResult: MPSTransactionResult?) {
+            this@PaymentActivity.runOnUiThread{
+                dialogHelper.hideLoadingDialog()
+                var body = ""
+                var toastAnswer = ""
+                //TODO add treatment to !! , when cannot have null branch
+                when(mpsTransactionResult!!.transactionStatus) {
+                    MPSTransactionResult.TransactionStatus.SUCCESS -> {
+                        body = resources.getString(R.string.cancelSuccess)
+                        toastAnswer = mpsTransactionResult.clientReceipt
+                    }
+                    MPSTransactionResult.TransactionStatus.ERROR -> {
+                        body = resources.getString(R.string.cancelError)
+                        toastAnswer = mpsTransactionResult.descriptionError
+                    }
+                }
+                dialogHelper.showTransactionDialog(this@PaymentActivity,
+                    mpsTransactionResult.transactionStatus.name,
+                    body
+                )
+                toast(toastAnswer)
+            }
+
+        }
+
+        override fun onDeconfigureAnswer(mpsTransactionResult: MPSTransactionResult?) {
+            this@PaymentActivity.runOnUiThread {
+                dialogHelper.hideLoadingDialog()
+                var body = ""
+                when(mpsTransactionResult!!.transactionStatus.name){
+                    MPSTransactionResult.TransactionStatus.SUCCESS.name -> body = resources.getString(R.string.deconfigureSuccess)
+                    MPSTransactionResult.TransactionStatus.ERROR.name->body = resources.getString(R.string.deconfigureError)
+                }
+                dialogHelper.showInitDialog(this@PaymentActivity,
+                    mpsTransactionResult.transactionStatus.name,
+                    body
+                )
+                toast(mpsTransactionResult.transactionStatus.name)
+
+            }
+        }
+
+        override fun onInitAnswer(mpsTransactionResult: MPSTransactionResult?) {
+            this@PaymentActivity.runOnUiThread {
+
+                dialogHelper.hideLoadingDialog()
+                var body = ""
+                when(mpsTransactionResult!!.transactionStatus.name){
+                    MPSTransactionResult.TransactionStatus.SUCCESS.name -> body = resources.getString(R.string.initSuccess)
+                    MPSTransactionResult.TransactionStatus.ERROR.name->body = resources.getString(R.string.initError)
+                }
+                dialogHelper.showInitDialog(this@PaymentActivity,
+                    mpsTransactionResult.transactionStatus.name,
+                    body
+                )
+                toast(mpsTransactionResult.transactionStatus.name)
+
+            }
+        }
+
+        override fun onTransactionAnswer(mpsTransactionResult: MPSTransactionResult?) {
+            this@PaymentActivity.runOnUiThread{
+                dialogHelper.hideLoadingDialog()
+                var body = ""
+                var toastAnswer = ""
+                when(mpsTransactionResult!!.transactionStatus) {
+                    MPSTransactionResult.TransactionStatus.SUCCESS -> {
+                        body = resources.getString(R.string.transactionSuccess)
+                        toastAnswer = mpsTransactionResult.clientReceipt
+                    }
+                    MPSTransactionResult.TransactionStatus.ERROR -> {
+                        body = resources.getString(R.string.transactionError)
+                        toastAnswer = mpsTransactionResult.descriptionError
+                    }
+                }
+                dialogHelper.showTransactionDialog(this@PaymentActivity,
+                    mpsTransactionResult.transactionStatus.name,
+                    body
+                )
+                toast(toastAnswer)
+            }
+        }
+
+        override fun onServiceDisconnected(componentName: ComponentName?) {
+            toast(componentName.toString())
+        }
+
+    }
+
+}
